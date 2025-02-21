9bcdc69e1f824b43f6e501666946661c908e74a4
lukasjai <lukas.jaiczay@stud.fh-campuswien.ac.at>
2025-02-21 12:57:32 +0100

    initial commit

---
+++ src/main/java/org/example/masterarbeit_github_action_test/MasterarbeitGithubActionTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.masterarbeit_github_action_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterarbeitGithubActionTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterarbeitGithubActionTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/masterarbeit_github_action_test/MasterarbeitGithubActionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.masterarbeit_github_action_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterarbeitGithubActionTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
