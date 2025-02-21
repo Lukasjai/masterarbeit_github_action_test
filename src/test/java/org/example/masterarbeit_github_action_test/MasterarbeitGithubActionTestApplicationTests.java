8a2b43b2b52cc1235c68cfe17ac811c2ce5af52b
lukasjai <lukas.jaiczay@stud.fh-campuswien.ac.at>
2025-02-21 13:06:38 +0100

    adding branch

--- src/test/java/org/example/masterarbeit_github_action_test/MasterarbeitGithubActionTestApplicationTests.java
+++ src/test/java/org/example/masterarbeit_github_action_test/MasterarbeitGithubActionTestApplicationTests.java
@@ -3,11 +3,18 @@ package org.example.masterarbeit_github_action_test;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class MasterarbeitGithubActionTestApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void test2(){
+        assertEquals(1,1);
     }
 
 }
