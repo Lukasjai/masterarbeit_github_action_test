3f26bbcac966efb2bdeb5a7be36d8429d8463283
lukasjai <lukas.jaiczay@stud.fh-campuswien.ac.at>
2025-03-27 09:29:02 +0100

    pipeline test 12

--- src/main/java/org/example/masterarbeit_github_action_test/controller/CalculatorController.java
+++ src/main/java/org/example/masterarbeit_github_action_test/controller/CalculatorController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class CalculatorController {
 
+
     private final CalculatorService calculatorService;
 
     public CalculatorController(CalculatorService calculatorService) {
