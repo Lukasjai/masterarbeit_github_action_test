e9b952956257059f977b3f6ab2ff259d59e82fa0
lukasjai <lukas.jaiczay@stud.fh-campuswien.ac.at>
2025-03-27 11:57:21 +0100

    pipeline test 21

--- src/main/java/org/example/masterarbeit_github_action_test/controller/CalculatorController.java
+++ src/main/java/org/example/masterarbeit_github_action_test/controller/CalculatorController.java
@@ -19,7 +19,6 @@ public class CalculatorController {
         return "calculator";
     }
 
-
     @GetMapping("/calculate")
     public String calculate(@RequestParam int a, @RequestParam int b, Model model) {
         int result = calculatorService.add(a, b);
