775c577025144db6fafb14cf0808df0845b29a55
lukasjai <lukas.jaiczay@stud.fh-campuswien.ac.at>
2025-04-16 14:57:17 +0200

    performance test locally 6

--- src/main/java/org/example/masterarbeit_github_action_test/MasterarbeitGithubActionTestApplication.java
+++ src/main/java/org/example/masterarbeit_github_action_test/MasterarbeitGithubActionTestApplication.java
@@ -15,7 +15,7 @@ public class MasterarbeitGithubActionTestApplication {
 
 
     @RequestMapping("/")
-    public String home() {return "Hello World Performance test 5 local";}
+    public String home() {return "Hello World Performance test 6 local";}
 
     //change
 }
